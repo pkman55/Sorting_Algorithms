d610b47dd43109520722988c1ffbb3db6cf47c5a
Albert <albertnaimovic16@gmail.com>
2020-11-26 01:52:48 +0200

    Bubble sort
    Adding bubble sort

---
+++ bubble.py
@@ -0,0 +1,6 @@
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
--- sorting_alg.py
+++ sorting_alg.py
@@ -1,27 +1,30 @@
 import numpy
 import math
-from datetime import datetime
-
-start = datetime.now()
+from insertion import insertionSort
+from bubble import bubbleSort
 
 pi = math.pi
 
-arr = numpy.array([12, 11, 13, 5, 6, pi, 87, 74, 5.47, 46, 87, 41, 65, 5/7, 1,
+unsorted_arr = numpy.array([12, 11, 13, 5, 6, pi, 87, 74, 5.47, 46, 87, 41, 65, 5/7, 1,
 44, 66, 88, 87, 95, 84, 62, 35, 14, 84, 5, 9, 7, 6, 3, 2, 4, 0, 8, 1, -1, -9,
 -98, -6574, -64, 741, -541, 69, 74, 85, 96, 4, 41, 52, 63, 69, 58, 47])
-def insertionSort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
 
-        j = i-1
-        while j >=0 and key < arr[j]:
-                arr[j+1] = arr[j]
-                j -= 1
-        arr[j+1] = key
+#for i in range(len(unsorted_arr)):
+#    print (unsorted_arr[i])
 
-insertionSort(arr)
-print ("Sorted array is:")
-for i in range(len(arr)):
-    print (arr[i])
-done = datetime.now()
-print("It took {} seconds".format(done-start))
+insertion_arr = unsorted_arr
+bubble_arr = unsorted_arr
+bubble_arr = numpy.insert(bubble_arr, len(bubble_arr), 69696969)
+
+insertionSort(insertion_arr)
+bubbleSort(bubble_arr)
+
+print ("(InsertionSort) Sorted array is:")
+for i in range(len(insertion_arr)):
+    print (insertion_arr[i])
+
+
+
+print ("(BubbleSort) Sorted array is:")
+for i in range(len(bubble_arr)):
+    print (bubble_arr[i])
